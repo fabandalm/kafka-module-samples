dcc3b2112ca05edac12952adafc9aba1e4d0ea85
Fabio Almeida <falmeida.tech@gmail.com>
2021-01-17 22:25:23 -0500

    Added Kafka streams sample

--- kafka-streams-sample/src/main/java/com/falmeida/tech/StreamsStarterApp.java
+++ kafka-streams-sample/src/main/java/com/falmeida/tech/StreamsStarterApp.java
@@ -2,8 +2,13 @@ package com.falmeida.tech;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class StreamsStarterApp {
@@ -15,6 +20,24 @@ public class StreamsStarterApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        //1 - Stream from Kafka
+        KStream<String,String> wordCountInput = builder.stream("word-count-input");
+        //2 - Map Values to lowercase
+        KTable<String,Long> wordCounts = wordCountInput.mapValues(value -> value.toLowerCase()).
+        //wordCountInput.mapValues(String::toLowerCase);
+        //3 - flatmap values split by space
+        flatMapValues(value -> Arrays.asList(value.split(""))).
+        //4
+        selectKey((ignored,word)-> word).
+        groupByKey().
+        count("Counts");
+
+        wordCounts.to(Serdes.String(),Serdes.Long(),"word-count-output");
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();;
+
     }
 
 }
