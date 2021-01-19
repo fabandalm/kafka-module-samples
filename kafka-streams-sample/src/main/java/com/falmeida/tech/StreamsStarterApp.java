a0f47a24f4934f8cc81faa636627e40156edfe7e
Fabio Almeida <falmeida.tech@gmail.com>
2021-01-18 23:23:41 -0500

    Finished stream class

--- kafka-streams-sample/src/main/java/com/falmeida/tech/StreamsStarterApp.java
+++ kafka-streams-sample/src/main/java/com/falmeida/tech/StreamsStarterApp.java
@@ -2,11 +2,15 @@ package com.falmeida.tech;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.KeyValueStore;
 
 import java.util.Arrays;
 import java.util.Properties;
@@ -25,18 +29,19 @@ public class StreamsStarterApp {
         //1 - Stream from Kafka
         KStream<String,String> wordCountInput = builder.stream("word-count-input");
         //2 - Map Values to lowercase
-        KTable<String,Long> wordCounts = wordCountInput.mapValues(value -> value.toLowerCase()).
-        //wordCountInput.mapValues(String::toLowerCase);
-        //3 - flatmap values split by space
-        flatMapValues(value -> Arrays.asList(value.split(""))).
-        //4
-        selectKey((ignored,word)-> word).
-        groupByKey().
-        count("Counts");
+        KTable<String,Long> wordCounts = wordCountInput
+                .mapValues(textLine -> textLine.toLowerCase())
+                //wordCountInput.mapValues(String::toLowerCase);
+                //3 - flatmap values split by space
+                .flatMapValues(lowercasedTextLine -> Arrays.asList(lowercasedTextLine.split("")))
+                //4
+                .selectKey((ignoredKey,word)-> word)
+                .groupByKey()
+                .count(Materialized.<String, Long, KeyValueStore<Bytes, byte[]>>as("Counts"));
 
-        wordCounts.to(Serdes.String(),Serdes.Long(),"word-count-output");
-        KafkaStreams streams = new KafkaStreams(builder, config);
-        streams.start();;
+        wordCounts.toStream().to("word-count-output", Produced.with(Serdes.String(),Serdes.Long()));
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
 
     }
 
