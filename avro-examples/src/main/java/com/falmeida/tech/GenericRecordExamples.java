d04664feb0ad08e5c417c1d0e131ad9162baaea9
Fabio Almeida <falmeida.tech@gmail.com>
2021-01-19 22:58:41 -0500

    Added new generic record sample

---
+++ avro-examples/src/main/java/com/falmeida/tech/GenericRecordExamples.java
@@ -0,0 +1,27 @@
+package com.falmeida.tech;
+
+import org.apache.avro.Schema;
+
+public class GenericRecordExamples {
+
+    public static void main(String[] args) {
+
+        //step 0: define schema
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse("{\n" +
+                "     \"type\": \"record\",\n" +
+                "     \"namespace\": \"com.example\",\n" +
+                "     \"name\": \"Customer\",\n" +
+                "     \"doc\": \"Avro Schema for our Customer\",     \n" +
+                "     \"fields\": [\n" +
+                "       { \"name\": \"first_name\", \"type\": \"string\", \"doc\": \"First Name of Customer\" },\n" +
+                "       { \"name\": \"last_name\", \"type\": \"string\", \"doc\": \"Last Name of Customer\" },\n" +
+                "       { \"name\": \"age\", \"type\": \"int\", \"doc\": \"Age at the time of registration\" },\n" +
+                "       { \"name\": \"height\", \"type\": \"float\", \"doc\": \"Height at the time of registration in cm\" },\n" +
+                "       { \"name\": \"weight\", \"type\": \"float\", \"doc\": \"Weight at the time of registration in kg\" },\n" +
+                "       { \"name\": \"automated_email\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the user is enrolled in marketing emails\" }\n" +
+                "     ]\n" +
+                "}");
+    }
+
+}
